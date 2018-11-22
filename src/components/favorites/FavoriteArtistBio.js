718be9af4181e0b2058194893c943b77a29e002e
github username <elis.paul7@gmail.com>
2018-11-22 14:39:25 -0500

    push to firebase

--- src/components/favorites/FavoriteArtistBio.js
+++ src/components/favorites/FavoriteArtistBio.js
@@ -6,7 +6,11 @@ class FavoriteArtistBio extends Component {
   renderArtistBio = () => {
     if(this.props.favorite_artists){
       for(let artist of this.props.favorite_artists){
-          return <p> {artist.bio.slice(0, 1000)} </p>
+        let content = artist.bio
+        // content = content.replace(/\n([ \t]*\n)+/g, <p></p>)
+        //          .replace(/\n/g, '<br />');
+        return <div> {content.slice(0, 1000)} </div>
+        // return <p> {artist.bio.slice(0, 1000)} </p>
       }
     }
   }
