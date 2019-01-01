b347ee1a23a2ff82688afb986eff08e588280b46
github username <elis.paul7@gmail.com>
2019-01-01 17:27:25 -0500

    adding spinner

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -53,15 +53,23 @@ class Favorites extends Component {
 
   render() {
     // console.log('in Favorites props', this.props);
-    // console.log('in Favorites state', this.state);
+    console.log('in Favorites state', this.state);
     return (
-      <div id={`${this.renderCSSTag()}favorites-container`}>
-      <div id='favorites-toplevel'>
-        <FavoriteArtistsList favorite_artists={this.state.favorite_artists} />
-        <FavoriteArtistCard favorite_artists={this.state.favorite_artists} deleteArtist={this.deleteArtist} />
-      </div>
-        <FavoriteArtistAlbums favorite_artists={this.state.favorite_artists} deleteAlbum={this.deleteAlbum} />
-        <FavoriteArtistBio favorite_artists={this.state.favorite_artists} />
+      <div>
+      { this.state.favorite_artists && this.state.favorite_artists.length ?
+        <div id={`${this.renderCSSTag()}favorites-container`}>
+          <div id='favorites-toplevel'>
+            <FavoriteArtistsList favorite_artists={this.state.favorite_artists} />
+            <FavoriteArtistCard favorite_artists={this.state.favorite_artists} deleteArtist={this.deleteArtist} />
+          </div>
+            <FavoriteArtistAlbums favorite_artists={this.state.favorite_artists} deleteAlbum={this.deleteAlbum} />
+            <FavoriteArtistBio favorite_artists={this.state.favorite_artists} />
+        </div>
+        : 
+        <div className='spinner-container'>
+          <div className="loader" />
+        </div>
+      }
       </div>
     );
   }
